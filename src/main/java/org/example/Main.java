414b7ec5eefa09c926734bc3533b169bb7700dc2
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-05-30 09:37:51 +0900

    work-1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+// 문제 : 아래와 같이 출력해주세요.
+// 문제 : 2부터 9까지의 숫자를 사용하지 않고 구구단 출력을 완성 해주세요.
+
+class Main {
+    public static void main(String[] args) {
+        int dan = 8;
+
+        // 수정가능지역 시작
+        int i = 1;
+
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        // 수정가능지역 끝
+
+    }
+}
