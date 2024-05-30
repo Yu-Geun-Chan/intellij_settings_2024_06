bf70b4ad3db3a23e073691068616d70c5b1a5a4f
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-05-30 12:59:06 +0900

    work-3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,107 @@
 package org.example;
 
-// 문제 : 아래와 같이 출력해주세요.
-// 문제 : 단을 * 1000까지 출력해주세요.
+//// 문제 : 아래와 같이 출력해주세요.
+//// 문제 : 단을 * 1000까지 출력해주세요.
+//
+//class Main {
+//    public static void main(String[] args) {
+//        int dan = 8;
+//
+//        int i = 1;
+//        while (i <= 1000) {
+//            System.out.println(dan + " * " + i + " = " + dan * i);
+//            i++;
+//        }
+//    }
+//}
 
-class Main {
-    public static void main(String[] args) {
-        int dan = 8;
+//// 문제 : 1부터 5까지 출력해주세요.
+//
+//class Main {
+//    public static void main(String[] args) {
+//        int i = 1;
+//        while(i <= 5) {
+//            System.out.println(i);
+//            i++;
+//        }
+//    }
+//}
 
-        int i = 1;
-        while (i <= 1000) {
-            System.out.println(dan + " * " + i + " = " + dan * i);
-            i++;
-        }
+//// 문제 : -100부터 25까지 출력해주세요.
+//
+//class Main {
+//    public static void main(String[] args) {
+//        int i = -100;
+//        while(i <= 25) {
+//            System.out.println(i);
+//            i++;
+//        }
+//    }
+//}
+
+//// 문제 : 아래와 같이 출력해주세요.
+//// 문제 : 단을 `* 1000`부터 `* -500`까지 출력해주세요.
+//
+//class Main {
+//    public static void main(String[] args) {
+//        int dan = 8;
+//
+//        int i = 1;
+//        while(i >= -500) {
+//            System.out.println(dan + " * " + i + " = " + dan * i);
+//            i--;
+//        }
+//    }
+//}
+
+//// 1부터 5까지의 합을 더한 결과를 출력해주세요.
+//
+//class Main {
+//    public static void main(String[] args) {
+//        int sum = 0;
+//
+//        int i = 1;
+//        while(i <= 5) {
+//            sum += i;
+//            i++;
+//        }
+//        System.out.println(sum);
+//    }
+//}
+
+//// -100부터 25까지의 합을 더한 결과를 출력해주세요.
+//
+//class Main {
+//    public static void main(String[] args) {
+//        int sum = 0;
+//        int i = -100;
+//        while (i <= 25) {
+//            sum += i;
+//            i++;
+//        }
+//        System.out.println(sum);
+//    }
+//}
+
+//// 문제 : 1부터 3까지 출력하는 작업을 10번 해주세요.
+//// 조건 : 2중 while문 사용
+//
+//class Main {
+//    public static void main(String[] args) {
+//        int i = 1;
+//        while (i <= 10) {
+//            int j = 1;
+//            i++;
+//            while (j <= 3) {
+//                System.out.println(j);
+//                j++;
+//            }
+//        }
+
+
     }
 }
+
+
+
+
