69e396112b64219bc7de018e39d711941908f74a
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-05-31 16:05:36 +0900

    work-6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,19 +5,18 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        int n = 10, firstTerm = 0, secondTerm = 1;
 
-        for (int i = 1; i <= n; ++i) {
-            System.out.println(firstTerm + ", ");
+        Scanner sc = new Scanner(System.in);
 
-            int nextTerm = firstTerm + secondTerm;
-            firstTerm = secondTerm;
-            secondTerm = nextTerm;
-            // 피보나치 수열
+        String s = sc.nextLine();
+        String[] arr = s.split("");
+
+        for (int i = 0; i <= arr.length - 1; i++) {
+            System.out.print("[" + arr[i] + "]");
         }
+
     }
 }
-
 
 
 
