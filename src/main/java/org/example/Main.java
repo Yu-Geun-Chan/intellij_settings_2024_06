83a2ee456f9a19620907849f3ae1f67165746adc
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-05-31 11:30:01 +0900

    work-5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,23 @@
 package org.example;
 
 
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
+        int n = 10, firstTerm = 0, secondTerm = 1;
 
+        for (int i = 1; i <= n; ++i) {
+            System.out.println(firstTerm + ", ");
 
-
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+            // 피보나치 수열
+        }
     }
 }
+
 
 
 
