b50b8971cb869c3c6096464d2d2cc9bb577c2b45
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-06-03 10:38:48 +0900

    6_03 work-1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,34 @@
 package org.example;
 
 
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        boolean[] arr1 = new boolean[3];
+        arr1[0] = true;
+        arr1[1] = false;
+        arr1[2] = true;
+        System.out.println(arr1[0]);
+        System.out.println(arr1[1]);
+        System.out.println(arr1[2]);
 
-        String s = sc.nextLine();
-        String[] arr = s.split("");
+        double[] arr2 = new double[3];
+        arr2[0] = 3.14;
+        arr2[1] = 7.77;
+        arr2[2] = 11.11;
+        System.out.println(arr2[0]);
+        System.out.println(arr2[1]);
+        System.out.println(arr2[2]);
 
-        for (int i = 0; i <= arr.length - 1; i++) {
-            System.out.print("[" + arr[i] + "]");
+        int[] arr3 = new int[10];
+        for (int i = 0; i <= arr3.length -1; i++) {
+            arr3[i] = i + 1;
+            System.out.printf("arr3 : %d\n", arr3[i]);
         }
 
     }
 }
+
 
 
 
