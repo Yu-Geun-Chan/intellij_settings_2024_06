76bdfe0efe18304228d4434173ba84467ab9f594
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-06-03 10:43:15 +0900

    6_03 work-2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,20 +8,22 @@ class Main {
         arr1[0] = true;
         arr1[1] = false;
         arr1[2] = true;
-        System.out.println(arr1[0]);
-        System.out.println(arr1[1]);
-        System.out.println(arr1[2]);
+        for (int i = 0; i <= arr1.length - 1; i++) {
+            System.out.printf("arr1 : %s\n", arr1[i]);
+        }
+
 
         double[] arr2 = new double[3];
         arr2[0] = 3.14;
         arr2[1] = 7.77;
         arr2[2] = 11.11;
-        System.out.println(arr2[0]);
-        System.out.println(arr2[1]);
-        System.out.println(arr2[2]);
+        for (int i = 0; i <= arr2.length - 1; i++) {
+            System.out.printf("arr2 : %s\n", arr2[i]);
+        }
+
 
         int[] arr3 = new int[10];
-        for (int i = 0; i <= arr3.length -1; i++) {
+        for (int i = 0; i <= arr3.length - 1; i++) {
             arr3[i] = i + 1;
             System.out.printf("arr3 : %d\n", arr3[i]);
         }
