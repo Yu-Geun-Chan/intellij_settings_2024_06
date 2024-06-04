f60a28385f2c6ab021c610200f6394b331db6175
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-06-04 16:38:44 +0900

    work-3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,28 +2,35 @@ package org.example;
 
 class Main {
     public static void main(String[] args) {
-        계산기.합(10, 20);
-        계산기.합(20, 50);
-        계산기.합(30, 40);
-
-//        계산기.합();
-//        계산기.합(true, 40);
-//        계산기.합(10, 40, 40);
-//        계산기.합(9);
 
 
+        int rs = 계산기.합(10, 20);
+        System.out.println(rs);
 
+        boolean rs2 = 계산기.is_bigger_than_b(10, 20);
+        System.out.println(rs2);
+        rs2 = 계산기.is_bigger_than_b(240, 20);
+        System.out.println(rs2);
+        rs2 = 계산기.is_bigger_than_b(20, 20);
+        System.out.println(rs2);
     }
 }
 
 class 계산기 {
-    int a = 40;
 
-    static void 합(int i, int j) {
-        System.out.println("i : " + i);
-        System.out.println("j : " + j);
+    static boolean is_bigger_than_b(int a, int b) {
+        if (a > b) {
+            return true;
+        }
+        return false;
     }
+
+    static int 합(int a, int b) {
+        return a + b;
+    }
+
 }
+
 
 
 
